848dd847095e66d067d914880524fe746a9c8504
Kenny <kananaya335@gmail.com>
2025-11-29 18:09:24 +0100

    done with the mini Dex

--- script.js
+++ script.js
@@ -11,98 +11,99 @@ async function fetchNewTokens() {
   });
   try {
     const data = await response.json();
-    console.log(data)
     const newestTokens = data.sort((a, b) => b.createdPoolAt - a.createdPoolAt)
     const infos = newestTokens.slice(0, 30);
 
 
     for (const token of infos) {
+
       const tokenId = token.tokenAddress;
-      const chainId = token.chainId.toUpperCase();
+      const description = token.description?.slice(0, 33);
+      const chainId = token.chainId.toLowerCase();
+      const realChain = chainId.slice(0, 3)
       const website = token.links?.find(s => s.label === 'Website')?.url;
       const telegram = token.links?.find(s => s.type === 'telegram')?.url;
       const twitter = token.links?.find(s => s.type === 'twitter')?.url;
       const resName = await fetch(`https://api.dexscreener.com/tokens/v1/${chainId}/${tokenId}`);
       const dataName = await resName.json();
       const imageURL = dataName[0].info.imageUrl;
-      const tokenName = dataName[0].baseToken.name;
-      //Creating new elements to append
+      const tokenName = dataName[0].baseToken.name.slice(0, 15);
 
-      const name = document.createElement('p')
-      name.id = 'name'
-      name.textContent = `${tokenName}/${chainId}`;
+
+      //Creating elements to append
+      const box = document.createElement('div')
+      box.classList.add('box')
+
+      const imageSec = document.createElement('div')
+      imageSec.className = 'image-sec'
 
       const img = document.createElement('img')
-      img.id = 'her'
+      img.id = 'wee'
       img.src = imageURL;
 
-      const nameBox = document.createElement('div')
-      nameBox.className = 'name-sec'
+      const blabsec = document.createElement('div')
+      blabsec.className = 'blab-sec'
 
-      const twitterIMG = document.createElement('img');
-      twitterIMG.src = 'twitter.png'
-      twitterIMG.className = 'dd'
+      const name = document.createElement('div')
+      name.className = 'name'
+      name.textContent = `${tokenName}/${realChain}`
 
-      const telegramIMG = document.createElement('img');
-      telegramIMG.src = 'tg.png'
-      telegramIMG.className = 'de'
+      const des = document.createElement('div')
+      des.className = 'description'
+      des.textContent = description + '...'
 
-      const websiteIMG = document.createElement('img');
-      websiteIMG.src = 'globe.png'
-      websiteIMG.className = 'dd'
+      const act = document.createElement('div')
+      act.className = 'action'
+
+      const twitterBox = document.createElement('div')
+      twitterBox.className = 'chain'
+      twitterBox.innerHTML = '<i class="fa-brands fa-x-twitter"></i>' + 'Twitter'
+
+      const telegramBox = document.createElement('div')
+      telegramBox.className = 'chain'
+      telegramBox.innerHTML = '<i class="fa-brands fa-telegram"></i>' + 'Telegram'
+
+      const websiteBox = document.createElement('div')
+      websiteBox.className = 'chain'
+      websiteBox.innerHTML = '<i class="fa-solid fa-globe"></i>' + 'Website'
 
       const twitterLink = document.createElement('a')
-      twitterLink.href = twitter;
       twitterLink.target = '_blank'
+      twitterLink.href = twitter
+      twitterLink.id = 'hh'
 
       const telegramLink = document.createElement('a')
-      telegramLink.href = telegram;
       telegramLink.target = '_blank'
+      telegramLink.href = telegram
+      telegramLink.id = 'hh'
 
-      const WebsiteLink = document.createElement('a')
-      WebsiteLink.href = website;
-      WebsiteLink.target = '_blank'
-
-      const socialBox = document.createElement('div')
-      socialBox.className = 'socials'
-
-      const box = document.createElement('div')
-      box.className = 'box'
-
-
-      //appending everything now
-      twitterLink.appendChild(twitterIMG)
-      telegramLink.appendChild(telegramIMG)
-      WebsiteLink.appendChild(websiteIMG)
-
-      socialBox.appendChild(twitterLink)
-      socialBox.appendChild(telegramLink)
-      socialBox.appendChild(WebsiteLink)
-
-      nameBox.appendChild(img)
-      nameBox.appendChild(name)
-
-      box.appendChild(nameBox)
-      box.appendChild(socialBox)
-
+      const websiteLink = document.createElement('a')
+      websiteLink.target = '_blank'
+      websiteLink.href = website
+      websiteLink.id = 'hh'
+      //testing append
+      imageSec.appendChild(img);
+      box.appendChild(imageSec)
+      blabsec.appendChild(name)
+      blabsec.appendChild(des)
+      twitterLink.appendChild(twitterBox)
+      telegramLink.appendChild(telegramBox)
+      websiteLink.appendChild(websiteBox)
+      act.appendChild(twitterLink)
+      act.appendChild(telegramLink)
+      act.appendChild(websiteLink)
+      blabsec.appendChild(act)
+      box.appendChild(blabsec)
       contentBox.appendChild(box)
 
-      if (!website) {
-        websiteIMG.style.opacity = '0.3'
-      }
-      if (!twitter) {
-        twitterIMG.style.opacity = '0.3'
-      }
-      if (!telegram) {
-        telegramIMG.style.opacity = '0.3'
-      }
+
     }
   }
   catch (err) {
-      console.error(err)
-    }
+    console.error(err)
+  }
 }
-refresh.addEventListener('click', () => {
-  contentBox.textContent = ''
-  fetchNewTokens()
-})
+
+
+
+fetchNewTokens()
